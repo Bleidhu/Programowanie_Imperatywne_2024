328522a4bdca8887cfc7afba89ce94b5ad08d1b5
Bleidhu <ichigosr2014@gmail.com>
2024-03-05 23:08:36 +0100

    Working zadanie 1

--- Zadania/Zadanie_1/Zadanie_1.c
+++ Zadania/Zadanie_1/Zadanie_1.c
@@ -1,12 +1,13 @@
-#include <cstdin>
+#include <stdio.h>
 
 void linspace(double v[], double start, double stop, int n)
 {
-    int dist = (stop - start)/n;
-
+    double dist = (stop - start)/(n- 1);
+    double val = 0;
     for(int i=0; i<n; ++i)
     {
-        v[i] = start + n;
+        v[i] = start + val;
+        val+=dist;
     }
 
 }
@@ -21,7 +22,12 @@ void add(double v1[], const double v2[], int n)
 
 double dot_product(const double v1[], const double v2[], int n)
 {
-
+    double ans = 0;
+    for(int i = 0; i < n; ++i)
+    {
+        ans+=v1[i]*v2[i];
+    }
+    return ans;
 }
 
 void multiply_by_scalar(double v[], int n, double s)
@@ -43,17 +49,81 @@ void range(double v[], int n, double start, double step)
 
 void read_vector(double v[], int n)
 {
-    
+    for(int i = 0; i < n; ++i)
+    {
+        scanf("%lf", &v[i]);
+    }
+}
+
+void print_vector(double v[], int n)
+{
+    for(int i = 0; i < n; ++i)
+    {
+        printf("%.2lf ", v[i]);
+    }
 }
 
 int main()
 {
-    int v1[] = {25, 50, 75, 100};
+    int command;
 
-    multiply_by_scalar(v1, 4, 2);
-    for(int i = 0; i < 4; ++i)
+    scanf("%d", &command);
+    
+
+   
+    if(command == 1)
     {
-        printf("%d", v1[i])
+        int n;
+        double start;
+        double stop;
+        scanf("%d %lf %lf", &n, &start, &stop);
+        double v[n];
+        linspace(v, start, stop, n);
+        print_vector(v, n);
+    
+    }
+    if(command == 2)
+    {
+        int n;
+        scanf("%d", &n);
+        double v1[n];
+        double v2[n];
+        read_vector(v1, n);
+        read_vector(v2, n);
+        add(v1, v2, n);
+        print_vector(v1, n);
+    
+    }
+    if(command == 3)
+    {
+        int n;
+        scanf("%d", &n);
+        double v1[n];
+        double v2[n];
+        read_vector(v1, n);
+        read_vector(v2, n);
+        double dotprod = dot_product(v1, v2, n);
+        printf("%.2lf", dotprod);
+    }
+    if(command == 4)
+    {
+        int n;
+        double s;
+        scanf("%d %lf", &n, &s);
+        double v[n];
+        read_vector(v, n);
+        multiply_by_scalar(v, n, s);
+        print_vector(v, n);
+    }
+    if(command == 5)
+    {
+        int n;
+        double start;
+        double step;
+        scanf("%d %lf %lf", &n, &start, &step);
+        double v[n];
+        range(v, n, start, step);
+        print_vector(v, n);
     }
     return 0;
 }
